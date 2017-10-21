73b06d4fd538e4e22ef86896a3554a4084667614
Javier Armero <Javalba90@gmail.com>
2017-10-21 15:31:37 +0200

    add design.js routes

---
+++ routes/designs.js
@@ -0,0 +1,182 @@
+let express = require('express');
+let router = express.Router();
+
+const Design = require('../models/design');
+const User = require('../models/user');
+
+const upload = require('../config/multer');
+const util = require('util');
+
+/* GET users listing. */
+
+router.get('/', function(req, res, next) {
+
+    Design.find({}, function(err, designList) {
+    if( err) {
+      res.json(err)
+    } else {
+      res.status(200).json(designList);
+    }
+  })
+});
+
+/**
+ * CREATE A NEW DESIGN
+ */
+router.post('/new', function(req, res, next) {
+    console.log(`DESIGNS NEW `);
+    console.log('body', req.body);
+  
+  let newDesign = new Design( {
+    creator: req.body.creator, //id
+    title: req.body.title,
+    designMainImg: req.body.designMainImg, //toDo: multer image
+    designGallery: req.body.designGallery, //array of images
+    description: req.body.description,
+    //image: `/uploads/${req.file.filename}` || ''
+  } )
+
+newDesign.save(function (err) {
+    if (err) {
+        res.json(err);
+    } else {
+        //INSERT DESIGN INTO DESIGNER
+        User
+            .findById(req.body.creator)
+            .populate('designerInfo.designs')
+            .exec((err, user) => {
+                if (err) {
+                    return next(err);
+                } else {
+
+                    if (user.role !== 'DESIGNER') {
+                        res.status(401).json({
+                            message: "DESIGNER ROLE REQUIRED",
+                        });
+                    } else {
+                        console.log(`user  before ----------------> ${user.designerInfo.designs}`);
+                        user.designerInfo.designs.push(newDesign._id);
+                        /*             console.log("newFigure--->: " + JSON.stringify(newFigure, null, 4));
+                        */
+                        user.save((err) => {
+                            if (err) {
+                                return next(err);
+                            } else {
+                                console.log(`user  after update ----------------> ${user.designerInfo.designs}`);
+
+                                res.json({
+                                    message: "design created & disign inserted",
+                                    design: newDesign
+                                });
+                            }
+                        });
+                    }
+
+                }
+            });
+    }
+})
+})
+
+/**
+ * VIEW A DESIGN
+ * 
+ * toRemove: Para que salga una ruta como esta 3887246-Meet-Wearly, en las rutas del client le paso como
+ * parametros la ruta que yo me quiera montar. 
+ * path: 'phones/:CustomId' , , como parametro le paso lo que me de la gana como string. 
+ */
+router.get('/:id', (req, res, next) => {
+
+    let idDesign = req.params.id;
+
+         console.log(`ID-:::::::::->${util.inspect(idDesign)}`);
+
+         //POPULATE CREATOR?
+/*             Design.findById(idDesign)
+            .populate('creator')
+            .exec((err, design) => { */
+
+    Design.findById(idDesign, (err, design) => {
+                if (!design) {
+                    res.status(401).json({
+                        message: "Error, no design founssd"
+                    });
+                } else {
+        
+/*                     //Only send relevant info. 
+                    let design = {
+                        id: design._id,
+                        designname: design.username,
+                        email: design.email,
+                        role: design.role,
+                        avatarUrl: design.avatarUrl,
+                        designInfo: {
+                            name: design.designInfo.name,
+                            surname: design.designInfo.surname,
+                            birthday: design.designInfo.birthday
+                        }
+                    }; */
+                    res.status(200).json({
+                        message: "design info",
+                        design: design
+                    });
+                }
+            })
+});
+
+
+
+/**
+ * EDIT A DESIGN
+ */
+router.put('/:id', function(req, res, next) {
+
+    let idDesign = req.params.id;
+    let designToUpdate = {
+        title: req.body.title,
+        designMainImg: req.body.designMainImg,
+        designGallery: req.body.designGallery, // array of images, not only one.
+        description: req.body.description,
+      }
+  
+    Design.findByIdAndUpdate(idDesign, designToUpdate, function(err){
+      if(err) {
+        res.json(err);
+      } else {
+        res.json({message: "updated"});
+      }
+    })
+  })
+
+
+/**
+ * DELETE A DESIGN
+ */
+router.delete('/:id', function(req, res, next) {
+
+  let idDesign = req.params.id;
+
+  console.log(`ENTRA EN DELETE`);
+  
+  console.log(`idDesign --> ${idDesign}`);
+  
+  Design.findById(idDesign, (err, design) => {
+      if (err) {
+          return next(err);
+      } else {
+          let idCreator = design.creator;
+          User.update({ _id: idCreator }, { "$pull": { "designerInfo.designs": idDesign} }, { safe: true }, (err, elem) => {
+            if (err){ return next(err);} 
+              Design.findByIdAndRemove(idDesign, (err, design) => {
+              if (err){ return next(err);} 
+              return res.json({
+                message: "design & id design user deleted"
+            });
+                    });
+          });
+      }
+  });
+
+})
+
+module.exports = router;
